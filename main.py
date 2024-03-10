e3dbcf09890278e420fef2b7bd063e235f49caf7
Teja Varma Pericharla <tpericharla@Tejas-MacBook-Air.local>
2024-03-10 20:50:27 +0530

    Teja's first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def game_guide():
+     print("Welcome to Adi's Bot game")
+def game_logic(goal):
+     while True:
+        input_number = int(input("Enter the number:"))
+
+        if input_number == 25:
+            print("You won the game")
+        else:  
+            print("You lost the game")
+            break
+
+goal = "More than 25"
+def main():
+    game_guide()
+    game_logic(25)
+
+
+if __name__ == "__main__":
+    main()
